bf1549cd5e602a66489f90f023e47c48d4d40588
nlozano <nathalylozano@uninorte.edu.co>
2024-05-01 16:56:19 -0500

    feat(app.js): Modificar app.

--- src/app.js
+++ src/app.js
@@ -2,3 +2,8 @@ const elemento = document.getElementById("miElemento");
 
 // Modifica el contenido del elemento
 elemento.textContent = "Este es el nuevo texto del elemento";
+
+const elemento = document.getElementById("miElemento");
+
+// Modifica el contenido del elemento
+elemento.textContent = "Este es el nuevo texto del elemento";
